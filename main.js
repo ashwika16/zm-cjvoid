77d3ef0031de837c42915ba9c464ecf0532712e9
ashwika16 <78404875+ashwika16@users.noreply.github.com>
2022-05-10 15:44:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function ab() {
+    window.location = "about him.html";
+}
+
+function photos() {
+    window.location = "photos.html";
+}
+
+function video() {
+    window.location = "videos.html";
+}
+function exit() {
+    window.location = "index.html";
+}
+function exit() {
+    window.location = "index.html";
+}
+function exit() {
+    window.location = "index.html";
+}
